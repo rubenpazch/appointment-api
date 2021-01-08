28be1f396c9b47862591e77b08d31730740ca708
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-08 11:02:59 -0500

    add versioning to the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # Api definition
-  namespace :api, defaults: { format: :json }  do 
+  namespace :api, defaults: { format: :json }  do
+    namespace :v1 do 
+    end 
   end
 end
