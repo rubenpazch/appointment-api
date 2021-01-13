f311a90523fbf5a420a03a40ea5efc1dae2b4ac2
Ruben Paz Chuspe <rubenpazchuspe@outlook.com>
2021-01-13 08:35:25 -0500

    Fix issues on rake test

--- test/models/role_test.rb
+++ test/models/role_test.rb
@@ -2,11 +2,11 @@ require 'test_helper'
 
 class RoleTest < ActiveSupport::TestCase
   test 'role with a valid name should be valid' do
-    role = Role.new(name: 'patient')
+    role = Role.new(name: 'patient', code: 'A')
     assert role.valid?
   end
   test 'role with a invalid name should be invalid' do
-    role = Role.new(name: '')
+    role = Role.new(name: '', code: 'A')
     assert_not role.valid?
   end
 end
